9b0539063387c8d6ed6f9900985a124f71fed404
nuskey8 <84110981+nuskey8@users.noreply.github.com>
2025-05-29 18:11:41 +0900

    fix: extension method for getting response data

--- src/GemiNet/GenerateContentResponseExtensions.cs
+++ src/GemiNet/GenerateContentResponseExtensions.cs
@@ -6,34 +6,52 @@ public static class GenerateContentResponseExtensions
     {
         if (response.Candidates == null) return null;
 
+        using var buffer = new PooledList<char>(1024);
+
+        var anyTextPartFound = false;
+
         foreach (var candidate in response.Candidates)
         {
             if (candidate.Content == null) continue;
 
             foreach (var part in candidate.Content.Parts)
             {
-                if (part.Text != null) return part.Text;
+                if (part.Text != null)
+                {
+                    buffer.AddRange(part.Text);
+                }
             }
         }
 
-        return null;
+        if (!anyTextPartFound) return null;
+
+        return buffer.AsSpan().ToString();
     }
 
-    public static Blob? GetData(this GenerateContentResponse response)
+    public static byte[]? GetData(this GenerateContentResponse response)
     {
         if (response.Candidates == null) return null;
 
+        using var buffer = new PooledList<byte>(1024);
+        var anyDataPartFound = false;
+
         foreach (var candidate in response.Candidates)
         {
             if (candidate.Content == null) continue;
 
             foreach (var part in candidate.Content.Parts)
             {
-                if (part?.InlineData != null) return part.InlineData;
+                if (part?.InlineData != null)
+                {
+                    buffer.AddRange(Convert.FromBase64String(part.InlineData.Data));
+                    anyDataPartFound = true;
+                }
             }
         }
 
-        return null;
+        if (!anyDataPartFound) return null;
+
+        return buffer.AsSpan().ToArray();
     }
 
     public static FunctionCall[] GetFunctionCalls(this GenerateContentResponse response)
@@ -80,5 +98,58 @@ public static class GenerateContentResponseExtensions
         }
 
         return null;
+    }
+
+    public static string? GetText(this BidiGenerateContentServerMessage message)
+    {
+        using var buffer = new PooledList<char>(1024);
+
+        if (message.ServerContent == null ||
+            message.ServerContent.ModelTurn == null ||
+            message.ServerContent.ModelTurn.Parts.Length == 0)
+        {
+            return null;
+        }
+
+        var anyTextPartFound = false;
+
+        foreach (var part in message.ServerContent.ModelTurn.Parts)
+        {
+            if (part.Text != null)
+            {
+                buffer.AddRange(part.Text);
+                anyTextPartFound = true;
+            }
+        }
+
+        if (!anyTextPartFound) return null;
+
+        return buffer.AsSpan().ToString();
+    }
+
+    public static byte[]? GetData(this BidiGenerateContentServerMessage message)
+    {
+        using var buffer = new PooledList<byte>(1024);
+
+        if (message.ServerContent == null ||
+            message.ServerContent.ModelTurn == null ||
+            message.ServerContent.ModelTurn.Parts.Length == 0)
+        {
+            return null;
+        }
+
+        var anyDataPartFound = false;
+
+        foreach (var part in message.ServerContent.ModelTurn.Parts)
+        {
+            if (part.InlineData != null)
+            {
+                buffer.AddRange(Convert.FromBase64String(part.InlineData.Data));
+            }
+        }
+
+        if (!anyDataPartFound) return null;
+
+        return buffer.AsSpan().ToArray();
     }
 }
