632c66e5216913fbba0806af8072be42b7dd8ab5
nuskey8 <84110981+nuskey8@users.noreply.github.com>
2025-05-30 01:25:50 +0900

    add: GoogleGenAIEmbeddingGenerator

---
+++ src/GemiNet.Extensions.AI/GoogleGenAIEmbeddingGenerator.cs
@@ -0,0 +1,62 @@
+using Microsoft.Extensions.AI;
+
+namespace GemiNet.Extensions.AI;
+
+public sealed class GoogleGenAIEmbeddingGenerator(GoogleGenAI ai, string model) : IEmbeddingGenerator<string, Embedding<float>>
+{
+    const int GeminiEmbeddingSize = 768;
+
+    public async Task<GeneratedEmbeddings<Embedding<float>>> GenerateAsync(
+        IEnumerable<string> values,
+        EmbeddingGenerationOptions? options = null,
+        CancellationToken cancellationToken = default)
+    {
+        var request = new EmbedContentRequest
+        {
+            Model = model,
+            Contents = new Content
+            {
+                Parts = [.. values.Select(v => new Part { Text = v })]
+            },
+            OutputDimensionality = options?.Dimensions,
+        };
+
+        var response = await ai.Models.EmbedContentAsync(request, cancellationToken);
+
+        var embeddingSize = options?.Dimensions ?? GeminiEmbeddingSize;
+
+        var vector = response.Embeddings
+            .SelectMany(x => x.Values)
+            .Select(x => (float)x)
+            .ToArray();
+
+        if (vector.Length % embeddingSize != 0)
+        {
+            throw new InvalidOperationException($"The returned embedding vector's size is not a multiple of the expected dimensions '{embeddingSize}'.");
+        }
+
+        var generatedEmbeddings = new GeneratedEmbeddings<Embedding<float>>(vector.Length / embeddingSize);
+
+        for (int i = 0; i < vector.Length; i += embeddingSize)
+        {
+            var embedding = new Embedding<float>(vector.AsMemory().Slice(i, embeddingSize));
+            generatedEmbeddings.Add(embedding);
+        }
+
+        return generatedEmbeddings;
+    }
+
+    public object? GetService(Type serviceType, object? serviceKey = null)
+    {
+        if (serviceType == typeof(GoogleGenAI))
+        {
+            return ai;
+        }
+
+        return null;
+    }
+
+    public void Dispose()
+    {
+    }
+}
